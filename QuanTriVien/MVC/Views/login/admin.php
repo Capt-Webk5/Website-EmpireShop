8f66633853a79c176ce51d48d0ff9e305d046954
VanPhuc1122 <Phucvv.23ns@vku.udn.vn>
2024-12-26 13:58:20 +0700

    LapTrinhWebShopEmpire

---
+++ QuanTriVien/MVC/Views/login/admin.php
@@ -0,0 +1,131 @@
+<div class="row">
+          <!-- ./col -->
+          <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+              <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Doanh Thu Tháng</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= number_format($data_countM['Count']) ?> VNĐ</div>
+                  </div>
+                  <div class="col-auto">
+                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- Doanh Thu Năm -->
+          <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+              <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Doanh Thu Năm </div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?= number_format($data_countY['Count']) ?> VNĐ</div>
+                  </div>
+                  <div class="col-auto">
+                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+            <!-- Người Dùng -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Người Dùng</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $data_nguoidung['Count'] ?></div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Nhân Viên -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Nhân Viên</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $data_nhanvien['Count'] ?></div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Đồng Hồ Nam -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Đồng Hồ Nam</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?=$data_tkdonghonam['Count']?></div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-clock fa-2x" ></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Đồng Hồ Nữ -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Đồng Hồ Nữ</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?=$data_tkdonghonu['Count']?></div>
+                    </div>
+                    <div class="col-auto">
+                    <i class="fas fa-clock fa-2x" ></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Đồng Hồ Cặp Đôi -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Đồng Hồ Cặp Đôi</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?=$data_tkdonghocapdoi['Count']?></div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-clock fa-2x" ></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Hoá Đơn Chưa được Duyệt -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Hoá Đơn Chưa Phê Duyệt</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?=$data_hd['Count']?></div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-comments fa-2x text-gray-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+
---
+++ views/footer/footer1.php
@@ -0,0 +1,139 @@
+<footer id="footer" class="footer-wrapper bg-gray">
+            <div class="container">
+                <div class="row footer-widget">
+                    <div class="col-sm-12 hidden-desktop hidden-tablet text-center mobile-info">
+                        <h2>Coppy Belong Văn Phúc</h2>
+                        <p><span class="icon-map-pin"></span>236 Ngũ Hành Sơn, Thành Phố Đà Nẵng</p>
+                        <p>TEL: <a href="tel:0985034632" title="Gọi 0985034632" rel="nofollow noopener">0985034632</a> | <a href="tel:0346593277" title="Gọi 0346593277" rel="nofollow noopener">0346593277</a> | <a href="tel: 0367293355" title="Gọi 0367293355" rel="nofollow noopener">0367293355</a></p>
+                        <div class="list-social">
+                            <ul class="social">
+                                <li><a href="" target="_blank" title="Facebook"><span class="icon-facebook2"></span></a>
+                                </li>
+                                <li><a href="" target="_blank" title="Tiktok"><span class="icon-tiktok-ko"></span></a>
+                                </li>
+                                <li><a href="" target="_blank" title="Youtube"><span class="icon-youtube1"></span></a>
+                                </li>
+                                <li><a href="" target="_blank" title="Instagram"><span class="icon-instagram"></span></a>
+                                </li>
+                                <li>
+                                <a href="index.php?url=login" class="icon-login">
+                                    <i class="bi bi-person-circle" style="transform: translateX(-25px);" title="Login Website"></i>
+                                </a>
+                                </li>
+                            </ul>
+                        </div>       
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-6">
+                        <h2 class="uppercase footer-title widget-title">Trợ giúp khách hàng</h2>
+                        <ul class="footer-link widget-content">
+                            <li><a href="" title="Chính sách bảo mật">Chính sách bảo mật</a>
+                            </li>
+                            <li><a href="" title="Chính sách vận chuyển">Chính sách vận chuyển</a>
+                            </li>
+                            <li><a href="" title="Chính sách hoàn tiền">Chính sách hoàn tiền</a>
+                            </li>
+                            <li><a href="" title="Bảo hành & sửa chữa đồng hồ">Bảo hành & sửa chữa đồng hồ</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-6">
+                        <h2 class="uppercase footer-title widget-title">Liên kết Website</h2>
+                        <ul class="footer-link widget-content">
+                            <li><a href="" title="Thương hiệu">Thương hiệu</a>
+                            </li>
+                            <li><a href="" title="Đồng hồ Nam">Đồng hồ Nam</a>
+                            </li>
+                            <li><a href="" title="Đồng hồ Nữ">Đồng hồ Nữ</a>
+                            </li>
+                            <li><a href="" title="Đồng hồ cặp đôi">Đồng hồ cặp đôi</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 col-lg-3 col-sm-12 hidden-mobile">
+                        <h2 class="uppercase footer-title widget-title">Tổng đài</h2>
+                        <div class="widget-content">
+                            <div class="footer-hotline">
+                                <p>Tư vấn mua hàng:</p>
+                                <p>
+                                    <a href="tel:0985034632" title="Hotline">0985034632</a>
+                                </p>
+                            </div>
+                            <div class="footer-cre">
+                                <h3 class="uppercase footer-title">Chứng nhận bởi</h3>
+                                <ul>
+                                    <li>
+                                        <a href="">
+                                            <img loading="lazy" src="/project/public/assets/image/ChungNhan/chungnhan-bct.svg" title="Chứng Nhận Bộ Công Thương">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="" target="_blank">
+                                            <img loading="lazy" src="/project/public/assets/image/ChungNhan/dmca.svg" title="DMCA">
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Theo Dõi Chúng Tôi -->
+                    <div class="col-md-6 col-lg-3 col-sm-12 hidden-mobile">
+                        <div class="list-social hidden-mobile">
+                            <h3 class="uppercase">Kết nối ngay</h3>
+                            <ul class="social">
+                                <li><a href="" target="_blank" title="Facebook"><span class="icon-facebook2"></span></a>
+                                </li>
+                                <li><a href="" target="_blank" title="Tiktok"><span class="icon-tiktok-ko"></span></a>
+                                </li>
+                                <li><a href="" target="_blank" title="Youtube"><span class="icon-youtube1"></span></a>
+                                </li>
+                                <li><a href="" target="_blank" title="Instagram"><span class="icon-instagram"></span></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- GG map -->
+                    <div class="col-md-12">
+                        <h2 class="uppercase footer-title widget-title">Vị Trí Cửa Hàng Chúng Tôi</h2>
+                            <div class="map-container">
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.991305088159!2d105.78515831538454!3d21.04192449346125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135abf38f5f355f%3A0x94c0d4f6756f5f1b!2sVietnam%20Korea%20University%20of%20Information%20and%20Communication%20Technology!5e0!3m2!1sen!2s!4v1666912072506!5m2!1sen!2s"
+                                    width="100%"
+                                    height="450"
+                                    style="border:0;"
+                                    allowfullscreen=""
+                                    loading="lazy"></iframe>
+                            </div>
+                        </div>
+                    </div>
+
+                    </div>
+                    <div class="hidden-desktop hidden-tablet footer-cre">
+                        <ul>
+                            <li>
+                                <a href="" target="_blank" rel="nofollow" title="Chứng nhận Bộ Công Thương">
+                                    <img loading="lazy" src="" width="100" height="38" title="Chứng nhận Bộ Công Thương" alt="Chung nhan bo cong thuong">
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dmca-badge" href="" target="_blank" title="DMCA">
+                                    <img loading="lazy" src="" title="DMCA" alt="DMCA">
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row footer-copyright hidden-mobile">
+                    <div class="col text-center">
+                        <p>
+                            © Coppy Belong Văn Phúc.<br>
+                            <span class="icon-map-pin"></span>236 Ngũ Hành Sơn, Thành Phố Đà Nẵng - Điện thoại: <a href="tel:0985034632" title="Gọi 0985034632" rel="nofollow noopener">0985034632</a> | <a href="tel:0346593277" title="Gọi 0346593277" rel="nofollow noopener">0346593277</a> | <a href="tel:0367293355" title="Gọi 0367293355" rel="nofollow noopener">0367293355</a> | Email: <a href="mailto:Phucvv.23ns@vku.udn.vn" title="Email" rel="nofollow noopener">Phucvv.23ns@vku.udn.vn</a>
+                        </p>
+                        <div class="offcanvas-header">
+                    </div>
+                </div>
+            </div>
+    <script defer src="https://cdn.luxshopping.vn/assets/bootstrap/js/bootstrap.min.js" id="bootstrap-js"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script defer src="https://cdn.luxshopping.vn/assets/js/sticksy.min.js" id="sticky-js"></script>
+    <script defer src="https://cdn.luxshopping.vn/assets/js/main.js?v=20" id="main-js"></script>  
+</footer>
